9c35bb74c4a1d651c2bdf6969c3d7c209b57a564
Ben McCarthy <bmccarthy90@gmail.com>
2025-02-06 12:05:55 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>HOME PAGE</p>
+      </div>
+    </>
+  )
+}
+
+export default App
