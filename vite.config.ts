133539e13696481d6a7d5d659baf00b5637f8ae7
ben mccarthy <bmccarthy90@gmail.com>
2025-02-17 22:30:37 -0500

    added base in config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // root: 'src',
-  // build: {
-  //   outDir: '../dist',
-  //   emptyOutDir: true, // also necessary
-  // }
+  base : "/Fetch-Take-Home"
 })
 
