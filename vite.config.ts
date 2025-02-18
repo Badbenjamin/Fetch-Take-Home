6da4690108e94b481be3509e1d9b115962a999e2
ben mccarthy <bmccarthy90@gmail.com>
2025-02-17 20:30:09 -0500

    nojekyll in src

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-  build: {
-    outDir: '../build',
-    emptyOutDir: true, // also necessary
-  }
+  // root: 'src',
+  // build: {
+  //   outDir: '../dist',
+  //   emptyOutDir: true, // also necessary
+  // }
 })
 
